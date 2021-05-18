d8e3009ba5c73dd1cb51d72cf6f7835cff6100ec
goreleaserbot <goreleaser@carlosbecker.com>
2021-05-18 16:06:59 +0200

    Brew formula update for dojoctl version v0.1.0-alpha.7

--- dojo.rb
+++ dojo.rb
@@ -5,28 +5,28 @@
 class Dojo < Formula
   desc ""
   homepage ""
-  version "0.1.0-alpha.6"
+  version "0.1.0-alpha.7"
   bottle :unneeded
 
   if OS.mac? && Hardware::CPU.intel?
-    url "https://github.com/zengineDev/dojo/releases/download/v0.1.0-alpha.6/dojoctl_0.1.0-alpha.6_Darwin_x86_64.tar.gz"
-    sha256 "7c6800dcf235d3ec76acc18235bf32cfa801d7235ac30b8bc30986e1d5e531b4"
+    url "https://github.com/zengineDev/dojo/releases/download/v0.1.0-alpha.7/dojoctl_0.1.0-alpha.7_Darwin_x86_64.tar.gz"
+    sha256 "939cfd656a0713020a18b6eb93993f44e72343bed5ecc48d95a22f4c979ba802"
   end
   if OS.mac? && Hardware::CPU.arm?
-    url "https://github.com/zengineDev/dojo/releases/download/v0.1.0-alpha.6/dojoctl_0.1.0-alpha.6_Darwin_arm64.tar.gz"
-    sha256 "0cf59293bf23d46929438f464e9f76d79379abce72b7e8044f6f43bc274cd5f5"
+    url "https://github.com/zengineDev/dojo/releases/download/v0.1.0-alpha.7/dojoctl_0.1.0-alpha.7_Darwin_arm64.tar.gz"
+    sha256 "339bbab5d8793bb175759d803599885eb337d62cac055b6c8c9043d350b9961a"
   end
   if OS.linux? && Hardware::CPU.intel?
-    url "https://github.com/zengineDev/dojo/releases/download/v0.1.0-alpha.6/dojoctl_0.1.0-alpha.6_Linux_x86_64.tar.gz"
-    sha256 "e1edfa52f9726bed2a170df7278e8f4ecfcf0e68c7cc97ada161084be98d0a1a"
+    url "https://github.com/zengineDev/dojo/releases/download/v0.1.0-alpha.7/dojoctl_0.1.0-alpha.7_Linux_x86_64.tar.gz"
+    sha256 "eaf14fd557d50ddfe3f616036e38fe4f409011a386fd2bc19cf46097997cc160"
   end
   if OS.linux? && Hardware::CPU.arm? && !Hardware::CPU.is_64_bit?
-    url "https://github.com/zengineDev/dojo/releases/download/v0.1.0-alpha.6/dojoctl_0.1.0-alpha.6_Linux_armv6.tar.gz"
-    sha256 "2f0abe841d9d03f388327f4f613cfdfd054b3830ce97222649e6814628d19069"
+    url "https://github.com/zengineDev/dojo/releases/download/v0.1.0-alpha.7/dojoctl_0.1.0-alpha.7_Linux_armv6.tar.gz"
+    sha256 "4f021eb274af70ae68da8164936e558ac9e5b3b5ca9ba556330c1c946e5a52f1"
   end
   if OS.linux? && Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-    url "https://github.com/zengineDev/dojo/releases/download/v0.1.0-alpha.6/dojoctl_0.1.0-alpha.6_Linux_arm64.tar.gz"
-    sha256 "e50703fd1282db4dede207b0dc84f87c523a42b19719d2716267883d0db72051"
+    url "https://github.com/zengineDev/dojo/releases/download/v0.1.0-alpha.7/dojoctl_0.1.0-alpha.7_Linux_arm64.tar.gz"
+    sha256 "d4fbd90ae085c05b5a14557fefa0a33fea7cc2630873c63137f1cc31e46ef0f1"
   end
 
   def install
